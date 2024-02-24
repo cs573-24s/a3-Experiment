80bd336596aa5b22e1b932779f52c2044cb141c7
chikpea123 <mcliao@wpi.edu>
2024-02-24 13:11:15 -0500

    Random data

--- app/src/pages/Vis1.jsx
+++ app/src/pages/Vis1.jsx
@@ -1,7 +1,24 @@
 export default function Vis1() {
 
-  return (
+  const generateRandomData = () => {
+    const data = [];
+    for (let i = 0; i < 5; i++) {
+      data.push({
+        x: Math.round(Math.random() * 100)
+      });
+    }
+    return data;
+  };
 
-    <div>vis1 page</div>
-  )
+  const randomData = generateRandomData();
+
+  console.log("Random Data:", randomData);
+
+  return (
+    <div>
+      <div>Data: {JSON.stringify(randomData)}</div>
+      <div>vis1 page</div>
+    </div>
+  );
 }
+
