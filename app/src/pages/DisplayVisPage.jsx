c05025c5624705f5647ed3d93b71ac56a7fe6da0
bhnord <b.nordemann@aol.com>
2024-02-25 22:31:49 -0500

    basic vis page

--- app/src/pages/DisplayVisPage.jsx
+++ app/src/pages/DisplayVisPage.jsx
@@ -1,12 +1,40 @@
-import { useState } from "react"
+import "./DisplayVisPage.css"
+import { useState, Children } from "react"
+import AnswerSelectoin from "../components/AnswerSelection"
+import AnswerSelection from "../components/AnswerSelection"
 export default function DisplayVisPage({ children }) {
-  const [currVis, changeVis] = useState(children[0])
+  const childrenArray = Children.toArray(children)
+  const [currVis, changeVis] = useState(childrenArray[0])
 
 
   //iterate over children 
+  const options = [1, 2, 3, 4, 5]
 
 
-  return <div>
-    {currVis}
-  </div>
+  //TODO: make look pretty
+
+  return (
+    <div>
+      <div id="progress-bar">
+        <div id="inner-progress" style={{ width: "10%" }} />
+      </div>
+      <div id="main-flex">
+        <button>left</button>
+        <div id="main-area">
+          {currVis}
+        </div>
+        <button>right</button>
+      </div>
+      <AnswerSelection options={options} />
+    </div>
+  )
+}
+
+
+const styles = {
+
+
+
+
+
 }
