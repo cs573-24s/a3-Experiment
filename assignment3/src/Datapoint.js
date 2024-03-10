b71b4838b072d38a1ca37a3f030f8a7c219566dd
abigailalbuquerque <122641078+abigailalbuquerque@users.noreply.github.com>
2024-03-10 16:00:02 -0400

    Fixing merge conflicts

---
+++ assignment3/src/Datapoint.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const style = {
+  fill: "steelblue",
+  fillOpacity: 0.7,
+  stroke: "steelblue",
+  strokeWidth: "1.5px"
+};
+
+const Datapoint = ({ x, y }) => {
+  return null;
+};
+
+export default Datapoint;
---
+++ assignment3/src/Scatterplot.js
@@ -0,0 +1,36 @@
+import React, { useMemo } from "react";
+import * as d3 from "d3";
+
+import Axis from "./Axis";
+
+// <circle cx={x} cy={y} r={3} />
+
+const Scatterplot = ({ x, y, width, height, data, datapoint }) => {
+  const xScale = d3.scaleLinear()
+    .domain([0, 100]) // TODO: change to parameter?
+    .range([0, width]);
+
+  // const yScale = useMemo(
+  //   () =>
+  //     d3
+  //       .scaleLinear()
+  //       .domain([0, 100])
+  //       .range([0, height]),
+  //   [height]
+  // );
+  const yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([0, height]);
+
+  return (
+    <g transform={`translate(${x}, ${y})`}>
+      {data.map(d => (
+        <circle key={d.id} cx={xScale(d.x)} cy={yScale(d.y)} r={3} fill={d.color} />
+      ))}
+      <Axis x={0} y={height} scale={xScale} />
+      <Axis x={0} y={0} scale={yScale} type={"Left"} />
+    </g>
+  );
+};
+
+export default Scatterplot;
