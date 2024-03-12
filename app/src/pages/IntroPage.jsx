4c510e10063b091dc9b53585a1b62229a9b17605
bhnord <b.nordemann@aol.com>
2024-03-12 11:34:24 -0400

    intro page spacing

--- app/src/pages/IntroPage.jsx
+++ app/src/pages/IntroPage.jsx
@@ -1,17 +1,18 @@
 import instructions from './intro-page.png'
 
 
-  //return must be ONE element (just wrap in div)
- console.log(instructions); // /logo.84287d09.png
-
 export default function IntroPage() {
 
-return (
+  return (
     <div>
-      <h2>Thank you for participating in our experiment! The following graphic provides instructions for completing the assesment. Once you are ready, click on the "Participate" button!</h2>
-      <img src={instructions} alt="Instructions" />
+      <h2 style={{ padding: "60px", paddingTop: "10px" }}>Thank you for participating in our experiment! The following graphic provides instructions for completing the assesment. Once you are ready, click on the "Participate" button!</h2>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ width: "60%", height: "60%" }}>
+          <img src={instructions} alt="Instructions" style={{ width: "100%", height: "100%", objectFit: "contain" }} />
+        </div>
+      </div>
     </div>
-  );  
+  );
 
 }
 
